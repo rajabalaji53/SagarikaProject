244c440a43243b6a6b24ef0c2a7ccc35d67ffa50
Sagarika Patel <sagarikap4@gmail.com>
2024-06-04 12:59:00 +0530

    pushing to git

---
+++ Program/src/Program1/UserProgram.java
@@ -0,0 +1,14 @@
+package Program1;
+
+public class UserProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Organization o=new Organization();
+		Employee e=new Employee();
+		o.addEmployee(001,"Sagarika");
+		e.employeedetails(001,"Sagarika",987654321, 340000);
+
+	}
+
+}
